7a9a1564f0786e4edcd3d42bafbc8274a584e9e8
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-08-27 09:47:54 +0200

    Add bulk label creation.

--- lib/sendcloud/resources/label.rb
+++ lib/sendcloud/resources/label.rb
@@ -3,5 +3,14 @@ module Sendcloud
     def retrieve(parcel_id:)
       Label.new get_request("labels/#{parcel_id}").body.dig("label")
     end
+
+    def retrieve_bulk(parcel_ids:)
+      attributes = {
+        label: {
+          parcels: parcel_ids
+        }
+      }
+      Label.new post_request("labels", body: attributes).body.dig("label")
+    end
   end
 end
