71619fc5a1703e51f25610eaabfca2d1a904d2ca
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-08-27 07:36:56 +0200

    Add retrieve for shipping methods

--- lib/sendcloud/resources/shipping_method.rb
+++ lib/sendcloud/resources/shipping_method.rb
@@ -4,5 +4,9 @@ module Sendcloud
       response = get_request("shipping_methods", params: params)
       Collection.from_response(response, key: "shipping_methods", type: ShippingMethod)
     end
+
+    def retrieve(shipping_method_id:)
+      ShippingMethod.new get_request("shipping_methods/#{shipping_method_id}").body.dig("shipping_method")
+    end
   end
 end
--- test/resources/shipping_method_test.rb
+++ test/resources/shipping_method_test.rb
@@ -9,4 +9,14 @@ class ShippingMethodResourceTest < Minitest::Test
     assert_equal Sendcloud::Collection, shipping_methods.class
     assert_equal Sendcloud::ShippingMethod, shipping_methods.data.first.class
   end
+
+  def test_retrieve
+    shipping_method_id = "8"
+    stub = stub_request("shipping_methods/#{shipping_method_id}", response: stub_response(fixture: "shipping_methods/retrieve"))
+    client = Sendcloud::Client.new(api_key: "key", api_secret: "secret", adapter: :test, stubs: stub)
+    shipping_method = client.shipping_method.retrieve(shipping_method_id: shipping_method_id)
+
+    assert_equal Sendcloud::ShippingMethod, shipping_method.class
+    assert_equal "sendcloud", shipping_method.carrier
+  end
 end
